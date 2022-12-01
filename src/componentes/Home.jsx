dbd25305eb79eb92d6b2a6cf0b16beee9429f743
Nmartinez28 <113324099+Nmartinez28@users.noreply.github.com>
2022-11-30 19:06:12 -0500

    Actualización Home

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,13 +1,29 @@
 import { useRef } from "react"
-
 export default function Home() {
+    const CotzRef = useRef();
+
+    function Cotizar() {
+        const Cotizacion = CotzRef.current.value;
+
+        fetch("http://localhost:8000", {
+            headers: { "content-type": "application/json" },
+            method: "POST",
+            body: JSON.stringify({ Cotizacion })
+        }).then(res => res.json())
+            .then(res => {
+                if (res.estado === "ok") {
+                    window.location.href = res.url;
+                }
+            })
+    }
+
     return (
         <form action="">
             <img src="src\assets\truck.PNG"></img>
             <p>
-                <label className="form-label" htmlFor="">Envios a todo destino</label>
+                <label className="form-label" htmlFor="">Envíos a todo destino</label>
             </p>
-            <button> Cotiza tu envio</button>
+            <button onClick={Cotizar} type="button" className="btn btn-primary">Cotiza tu envío</button>
         </form>
     )
 }

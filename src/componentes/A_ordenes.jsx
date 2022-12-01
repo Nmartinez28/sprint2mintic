cba0133480d3428b6fbf74607862c8652a742ef8
Nmartinez28 <113324099+Nmartinez28@users.noreply.github.com>
2022-11-30 19:04:15 -0500

    Actualización A_ordenes

--- src/componentes/A_ordenes.jsx
+++ src/componentes/A_ordenes.jsx
@@ -1,6 +1,66 @@
-export default function Registro_ordenes(){
+import { useRef } from "react"
+
+export default function A_ordenes(){
+    const NameRef = useRef();
+    const IDRef = useRef();
+    const TlfRef = useRef();
+    const DateRef = useRef();
+    const TimeRef = useRef();
+    const StateRef = useRef();
+    const LongRef = useRef();
+    const WidthRef = useRef();
+    const HeightRef = useRef();
+    const WeightRef = useRef();
+    const Address1Ref = useRef();
+    const City1Ref = useRef();
+    const Address2Ref = useRef();
+    const City2Ref = useRef();
+
+    function Actualizar_orden() {
+        const name = NameRef.current.value;
+        const id = IDRef.current.value;
+        const tlf = TlfRef.current.value;
+        const fecha = DateRef.current.value;
+        const hora = TimeRef.current.value;
+        const estado = StateRef.current.value;
+        const largo = LongRef.current.value;
+        const ancho = WidthRef.current.value;
+        const altura = HeightRef.current.value;
+        const peso = WeightRef.current.value;
+        const ciudad = City1Ref.current.value;
+        const direccion = Address1Ref.current.value;
+        const ciudad2 = City2Ref.current.value;
+        const direccion2 = Address2Ref.current.value;
+
+        fetch("http://localhost:8000/A_ordenes", {
+            headers: {
+                "content-type": "application/json", //TIPO MIME
+            },
+            method: "POST",
+            body: JSON.stringify({ name, id, tlf, fecha, hora, estado, largo, ancho, altura, peso, ciudad, direccion, ciudad2, direccion2 })
+        })
+            .then(data => data.json())
+            .then(res => {
+                if (res.estado == "ok") {
+                    window.location.href = res.url;
+                }
+            })
+    }
+
     return(
         <form action="">
+            <p>
+                <label className="form-label" htmlFor="">Nombre del destinatario</label>
+                <input className="form-control" type="Text"/>
+            </p>
+            <p>
+                <label className="form-label" htmlFor="">Cédula/nit del destinatario</label>
+                <input className="form-control" type="Text"/>
+            </p>
+            <p>
+                <label className="form-label" htmlFor="">Teléfono de contacto</label>
+                <input className="form-control" type="Tel"/>
+            </p>
             <p>
                 <label className="form-label" htmlFor="">Fecha</label>
                 <input className="form-control" type="date"/>
@@ -17,60 +77,106 @@ export default function Registro_ordenes(){
                 </select>
             </p>
             <p>
-                <label className="form-label" htmlFor="">Largo </label>
+                <label className="form-label" htmlFor="">Largo</label>
                 <input className="form-control" type="number"/>
             </p>  
             <p>
-                <label className="form-label" htmlFor="">Ancho </label>
+                <label className="form-label" htmlFor="">Ancho</label>
                 <input className="form-control" type="number"/>
             </p>
             <p>
-                <label className="form-label" htmlFor="">Alto </label>
+                <label className="form-label" htmlFor="">Alto</label>
                 <input className="form-control" type="number"/>
             </p>     
             <p>
-                <label className="form-label" htmlFor="">Peso </label>
+                <label className="form-label" htmlFor="">Peso</label>
                 <input className="form-control" type="number"/>
             </p>
             <p>
-                <label className="form-label" htmlFor="">Dirección de recogida </label>
+                <label className="form-label" htmlFor="">Dirección de recogida</label>
                 <input className="form-control" type="Text"/>
             </p>
             <p>
-            <label className="form-label" htmlFor="">Ciudad de Recogida </label>
+            <label className="form-label" htmlFor="">Ciudad de recogida</label>
                 <select className="form-control" name="C_recogida" id="C_recogida">
-                    <option value="Bogota">Bogotá</option>
-                    <option value="Medellin">Medellín</option>
-                    <option value="Cali">Cali</option>
+                    <option value=""></option>
+                    <option value="Arauca">Arauca</option>
+                    <option value="Armenia">Armenia</option>
                     <option value="Barranquilla">Barranquilla</option>
-                    <option value="Cartagena">Cartagena</option>
+                    <option value="Bogotá">Bogotá D.C.</option>
                     <option value="Bucaramanga">Bucaramanga</option>
+                    <option value="Cali">Cali</option>
+                    <option value="Cartagena">Cartagena</option>
+                    <option value="Cúcuta">Cúcuta</option>
+                    <option value="Florencia">Florencia</option>
+                    <option value="Ibagué">Ibagué</option>
+                    <option value="Inírida">Inírida</option>
+                    <option value="Leticia">Leticia</option>
+                    <option value="Manizales">Manizales</option>
+                    <option value="Medellín">Medellín</option>
+                    <option value="Mitú">Mitú</option>
+                    <option value="Mocoa">Mocoa</option>
+                    <option value="Montería">Montería</option>
+                    <option value="Neiva">Neiva</option>
+                    <option value="Quibdó">Quibdó</option>
+                    <option value="Pasto">Pasto</option>
+                    <option value="Pereira">Pereira</option>
+                    <option value="Popayán">Popayán</option>
+                    <option value="Bucaramanga">Puerto Carreño</option>
+                    <option value="Riohacha">Riohacha</option>
+                    <option value="San Andrés Isla">San Andrés Isla</option>
+                    <option value="Santa Marta">Santa Marta</option>
+                    <option value="San José del Guaviare">San José del Guaviare</option>
+                    <option value="Sincelejo">Sincelejo</option>
+                    <option value="Tunja">Tunja</option>
+                    <option value="Valledupar">Valledupar</option>
+                    <option value="Villavicencio">Villavicencio</option>
+                    <option value="Yopal">Yopal</option>
                 </select>
             </p>
             <p>
-                <label className="form-label" htmlFor="">Nombre destinatario </label>
+                <label className="form-label" htmlFor="">Dirección de entrega</label>
                 <input className="form-control" type="Text"/>
             </p>
             <p>
-                <label className="form-label" htmlFor="">Cédula/nit destinatario </label>
-                <input className="form-control" type="Text"/>
-            </p>
-            <p>
-                <label className="form-label" htmlFor="">Dirección de entrega </label>
-                <input className="form-control" type="Text"/>
-            </p>
-            <p>
-                <label className="form-label" htmlFor="">Ciudad de Entrega </label>
+                <label className="form-label" htmlFor="">Ciudad de entrega</label>
                 <select className="form-control" name="C_entrega" id="C_entrega">
-                    <option value="Bogota">Bogotá</option>
-                    <option value="Medellin">Medellín</option>
-                    <option value="Cali">Cali</option>
+                    <option value=""></option>
+                    <option value="Arauca">Arauca</option>
+                    <option value="Armenia">Armenia</option>
                     <option value="Barranquilla">Barranquilla</option>
-                    <option value="Cartagena">Cartagena</option>
+                    <option value="Bogotá">Bogotá D.C.</option>
                     <option value="Bucaramanga">Bucaramanga</option>
+                    <option value="Cali">Cali</option>
+                    <option value="Cartagena">Cartagena</option>
+                    <option value="Cúcuta">Cúcuta</option>
+                    <option value="Florencia">Florencia</option>
+                    <option value="Ibagué">Ibagué</option>
+                    <option value="Inírida">Inírida</option>
+                    <option value="Leticia">Leticia</option>
+                    <option value="Manizales">Manizales</option>
+                    <option value="Medellín">Medellín</option>
+                    <option value="Mitú">Mitú</option>
+                    <option value="Mocoa">Mocoa</option>
+                    <option value="Montería">Montería</option>
+                    <option value="Neiva">Neiva</option>
+                    <option value="Quibdó">Quibdó</option>
+                    <option value="Pasto">Pasto</option>
+                    <option value="Pereira">Pereira</option>
+                    <option value="Popayán">Popayán</option>
+                    <option value="Bucaramanga">Puerto Carreño</option>
+                    <option value="Riohacha">Riohacha</option>
+                    <option value="San Andrés Isla">San Andrés Isla</option>
+                    <option value="Santa Marta">Santa Marta</option>
+                    <option value="San José del Guaviare">San José del Guaviare</option>
+                    <option value="Sincelejo">Sincelejo</option>
+                    <option value="Tunja">Tunja</option>
+                    <option value="Valledupar">Valledupar</option>
+                    <option value="Villavicencio">Villavicencio</option>
+                    <option value="Yopal">Yopal</option>
                 </select>
             </p>
-            <button>Actualizar orden</button>
-        </form>    
-    )
-}
+            <button type="button" onClick={Actualizar_orden} className="btn btn-primary">Actualizar orden</button>
+        </form>
+        )
+    }
